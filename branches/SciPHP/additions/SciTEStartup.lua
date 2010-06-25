03b31fc8b9d1a0f4e16d2d7c5e5a547e4911abb7
long2513 <long2513@1696db6e-c365-11de-ace0-0fc9cb3442a6>
2010-06-25 13:02:55 +0000

    add files

---
+++ branches/SciPHP/additions/SciTEStartup.lua
@@ -0,0 +1,13 @@
+-------------------------
+-- Import lua files
+-------------------------
+-- Common.lua
+dofile (props["SciteDefaultHome"].."\\additions\\Common.lua")
+--SideBar
+dofile (props["SciteDefaultHome"].."\\additions\\SideBar.lua")
+--AutoComplete
+dofile (props["SciteDefaultHome"].."\\additions\\AutoComplete.lua")
+--Snippets
+dofile (props["SciteDefaultHome"].."\\additions\\Snippets.lua")
+--PairedTags
+dofile (props["SciteDefaultHome"].."\\additions\\PairedTags.lua")
